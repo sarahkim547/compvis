c3f22c6179e45eb988839a155a2c4a8023029dc8
Nate Brennan <nathaniel_brennan@brown.edu>
2019-05-01 21:07:29 -0400

    training code

--- network.py
+++ network.py
@@ -1,5 +1,8 @@
 from keras.layers import Conv2D, Dense, Dropout, MaxPool2D
 from keras.models import Sequential
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
 
 model = Sequential()
 model.add(Conv2D(filters=25, kernel_size=4, activation='relu'))
@@ -16,3 +19,43 @@ model.add(Dropout(rate=0.5))
 model.add(Dense(units=1024, activation='relu'))
 model.add(Dropout(rate=0.5))
 model.add(Dense(units=3, activation='softmax'))
+
+NUM_TRAINING_EXAMPLES = 491220
+BATCH_SIZE = 20
+NUM_EPOCHS = 1
+
+class DataGenerator():
+
+    def __init__(self, labels, batch_size):
+        self.labels = labels
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return np.ceil(NUM_TRAINING_EXAMPLES / float(self.batch_size))
+
+    def __getitem__(self, idx):
+        start_idx = idx * self.batch_size
+        end_idx = (idx + 1) * self.batch_size
+
+        batch_x = []
+        for i in range(start_idx, end_idx):
+            if i < NUM_TRAINING_EXAMPLES:
+                batch_x.append(np.load('patches/{:07d}.npy'.format(i)))
+
+        batch_x = np.stack(batch_x)
+        batch_y = self.labels[start_idx:end_idx]
+
+        return batch_x, batch_y
+
+
+labels = np.load('labels.npy')
+training_batch_generator = DataGenerator(labels, BATCH_SIZE)
+
+model.fit_generator(generator=training_batch_generator,
+                    steps_per_epoch=(NUM_TRAINING_EXAMPLES // BATCH_SIZE),
+                    epochs=NUM_EPOCHS,
+                    verbose=1,
+                    # validation_data=my_validation_batch_generator,
+                    # validation_steps=(num_validation_samples // batch_size),
+                    use_multiprocessing=True,
+                    workers=16)
