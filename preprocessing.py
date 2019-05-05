0f57bb300bfd360326ae1cfb5518ad19b39eeef4
Nate Brennan <nathaniel_brennan@brown.edu>
2019-05-05 10:48:17 -0400

    preprocessing can take a sample of data

--- preprocessing.py
+++ preprocessing.py
@@ -4,7 +4,12 @@ This file contains all of the functions for pre-processing the data
 import argparse
 import numpy as np
 import os
+import random
 from skimage import io
+
+
+PATCH_SIZE = 51
+PAD = int((PATCH_SIZE - 1) / 2)
 
 
 def normalizeImage(image):
@@ -16,31 +21,34 @@ def normalizeImage(image):
     return (image.astype(np.float32) - 128) / 128
 
 
-def extractPatches(im, mask, patch_num, patch_dir):
-    assert im.shape[:2] == mask.shape
+def extractPatches(im, patch_num, sample_num, patch_dir, sample=None):
+    h, w = im.shape[0], im.shape[1]
+    for i in range(PAD, h - PAD, 2):
+        for j in range(PAD, w - PAD, 2):
+            if sample is None or patch_num in sample:
+                curr_patch = im[i-PAD:i+PAD+1, j-PAD:j+PAD+1, :]
+                patch_file = os.path.join(patch_dir, '{:07d}'.format(sample_num))
+                np.save(patch_file, curr_patch)
+                sample_num += 1
+            patch_num += 1
+
+    return patch_num, sample_num
+
+
+def getLabels(mask):
+    mask //= 127  # Convert {0, 128, 255} to {0, 1, 2}
     h, w = mask.shape
-
-    patch_size = 51
-    pad = int((patch_size - 1) / 2)
-    num_patches = 0
-    for i in range(pad, h - pad, 2):
-        for j in range(pad, w - pad, 2):
-            curr_patch = im[i-pad:i+pad+1, j-pad:j+pad+1, :]
-            patch_file = os.path.join(patch_dir, '{:07d}'.format(patch_num + num_patches))
-            np.save(patch_file, curr_patch)
-            num_patches += 1
-
-    labels = mask[pad:h - pad:2, pad:w - pad:2]
-    labels = labels.flatten()
-    assert num_patches == len(labels)
-
-    return labels
+    labels = mask[PAD:h - PAD:2, PAD:w - PAD:2]
+    return labels.flatten()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data', choices=['train', 'test'], required=True, help='Process train or test dataset.')
+    parser.add_argument('--num', type=int, default=0, help='Number of examples to process (0 = all examples).')
     args = parser.parse_args()
+    if args.num < 0:
+        raise ValueError('Num must be a nonnegative integer.')
 
     data_dir = 'train_data' if args.data == 'train' else 'test_data'
     pic_dir = os.path.join(data_dir, 'pics')
@@ -49,23 +57,32 @@ def main():
     if not os.path.exists(patch_dir):
         os.mkdir(patch_dir)
 
+    print('Processing labels')
     all_labels = []
-    patch_num = 0
-    for pic_file in os.listdir(pic_dir):
+    for pic_file in sorted(os.listdir(pic_dir)):
+        pic_name, _ = os.path.splitext(pic_file)
+        mask_file = 'TM_' + pic_name + '.png'
+        mask = io.imread(os.path.join(mask_dir, mask_file))
+        labels = getLabels(mask)
+        all_labels.append(labels)
+
+    all_labels = np.concatenate(all_labels)
+    if args.num == 0 or args.num > len(all_labels):
+        np.save(os.path.join(data_dir, 'labels'), all_labels)
+        sample = None
+    else:
+        sample = random.sample(range(len(all_labels)), args.num)
+        sample.sort()
+        sampled_labels = all_labels[sample]
+        np.save(os.path.join(data_dir, 'labels'), sampled_labels)
+        sample = set(sample)
+
+    patch_num, sample_num = 0, 0
+    for pic_file in sorted(os.listdir(pic_dir)):
         print('Processing image: {}'.format(pic_file))
         im = io.imread(os.path.join(pic_dir, pic_file))
         im = normalizeImage(im)
-
-        pic_name, _ = os.path.splitext(pic_file)
-        mask_file = 'TM_' + pic_name + '.png'
-        mask = io.imread(os.path.join(mask_dir, mask_file)) // 127
-
-        labels = extractPatches(im, mask, patch_num, patch_dir)
-        all_labels.append(labels)
-        patch_num += len(labels)
-
-    all_labels = np.concatenate(all_labels)
-    np.save(os.path.join(data_dir, 'labels'), all_labels)
+        patch_num, sample_num = extractPatches(im, patch_num, sample_num, patch_dir, sample)
 
 
 if __name__ == '__main__':
