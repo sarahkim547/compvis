f4f7743b58d7d49f87719a3d40460ad05396902d
Nate Brennan <nathaniel_brennan@brown.edu>
2019-05-05 16:23:53 -0400

    .

--- network.py
+++ network.py
@@ -84,8 +84,8 @@ def main():
         raise ValueError('Epochs must be a positive integer.')
 
     model, lr_schedule = create_model()
-    train_labels = to_categorical(np.load(os.path.join(args.train_dir, 'labels.npy')))
-    test_labels = to_categorical(np.load(os.path.join(args.test_dir, 'labels.npy')))
+    train_labels = to_categorical(np.load(os.path.join(args.train_dir, 'labels.npy')), num_classes=3)
+    test_labels = to_categorical(np.load(os.path.join(args.test_dir, 'labels.npy')), num_classes=3)
     train_batch_generator = DataGenerator(os.path.join(args.train_dir, 'patches'),
                                           train_labels, args.num_train, BATCH_SIZE)
     test_batch_generator = DataGenerator(os.path.join(args.test_dir, 'patches'),
