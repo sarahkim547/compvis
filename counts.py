e7c2eb97e096c998e6dfce6e8f2e65e9c6909b44
Nate Brennan <nathaniel_brennan@brown.edu>
2019-05-05 21:37:56 -0400

    counts script

--- counts.py
+++ counts.py
@@ -6,5 +6,5 @@ parser = argparse.ArgumentParser()
 parser.add_argument('ar', nargs=1)
 args = parser.parse_args()
 
-ar = np.load(args.ar)[0]
+ar = np.load(args.ar[0])
 print(dict(zip(*np.unique(ar, return_counts=True))))
