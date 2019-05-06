5602da54cd94f71c82eef009d6c75c1ea2cd89de
Nate Brennan <nathaniel_brennan@brown.edu>
2019-05-06 10:47:35 -0400

    small change

--- counts.py
+++ counts.py
@@ -3,10 +3,13 @@ import numpy as np
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('ar', nargs=1)
+parser.add_argument('--ar', required=True)
 args = parser.parse_args()
 
-ar = np.load(args.ar[0])
+ar = np.load(args.ar)
+if len(ar.shape) == 3:
+    ar = np.argmax(ar, axis=2)
+ar = ar.flatten()
 print(len(ar))
 counts = dict(zip(*np.unique(ar, return_counts=True)))
 print(counts)
